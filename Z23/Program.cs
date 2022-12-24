ec276d3152ebf49b1a90f28207843fedfabe6b27
alexapetrova <mal.alek.97@gmail.com>
2022-12-24 14:51:48 +0300

    2 задачи сделаны

--- Z23/Program.cs
+++ Z23/Program.cs
@@ -1,2 +1,19 @@
 ﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 //3 -> 1, 8, 27, 5 -> 1, 8, 27, 64, 125
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int N =  getUserValue("Введите число");
+
+for (int i = 1; i<=N; i++)
+{
+    if (i<N)
+    {Console.Write(Math.Pow(i,3) + ", " );}
+    else
+    {Console.Write(Math.Pow(i,3) + ". " );}
+}
