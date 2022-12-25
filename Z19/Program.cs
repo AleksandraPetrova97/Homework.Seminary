eb0da206c6b52a5f3b379dd8483420700b0513de
alexapetrova <mal.alek.97@gmail.com>
2022-12-25 14:46:46 +0300

    сделала домашнюю работу

--- Z19/Program.cs
+++ Z19/Program.cs
@@ -1,3 +1,18 @@
 ﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 //14212 -> нет, 12821 -> да
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+int num =  getUserValue("Введите число");
 
+if (num/10000 == num%10 && num/1000%10 == num/10%10)
+{
+    Console.WriteLine($"{num} - да" );
+}
+else 
+{
+    Console.WriteLine($"{num} - нет" );
+}
