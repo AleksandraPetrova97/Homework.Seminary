ba089589d27ec8c8b585c89295ace60cf6ba902f
Aleksandra Petrova <mal.alek.97@gmail.com>
2023-01-14 12:28:47 +0300

    Задача 25 решена

---
+++ Z25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵) 2, 4 -> 16
+
+
+int getUserNumber(string message)
+{
+    Console.WriteLine(message);
+    int UserNumber = int.Parse(Console.ReadLine());
+    return UserNumber;
+}
+
+int numA = getUserNumber("Введите число А");
+int numB = getUserNumber("Введите число B");
+
+
+int getdegreeOfRange(int numA, int numB)
+{
+    int degree = numA;
+    for (int i = 1; i < numB; i++)
+    {
+        degree = degree * numA;
+    }
+    return degree;
+} 
+
+int degree = getdegreeOfRange(numA, numB);
+Console.WriteLine($"{degree}");
